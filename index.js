e1bba0764d26dafaffa8babf92ad98661ad971a8
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-05-08 15:40:28 +0530

    fixed engine

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ config({ path: './data/config.env' })
 app.use(express.json()) ;
 app.use(cookieParser()) ;
 
-app.use("/api/v1/users",userRoutes);
-app.use("/api/v1/task",taskRoutes);
-app.use(cors({
+app.use(cors({ 
   origin:[process.env.FRONTEND_URL],
   methods:["GET","POST","PUT","DELETE"],
   credentials:true,
 }))
 
 
+app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/task",taskRoutes);
 //Error handling
 app.use(errorHandlerFunc)
 
